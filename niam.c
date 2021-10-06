5e5a6637c156e7a33706ad3856554efe9a5b7b80
tomas-20 <tacuna20@stuy.edu>
2021-10-05 22:03:08 -0400

    created an array with random values except for the initial 0

--- niam.c
+++ niam.c
@@ -2,5 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 int main() {
+  int array[10];
+  array[0] = 0;
+  srand(time(NULL));
+  for (int i = 1; i < 10; i ++) {
+    array[i] = rand();
+  }
   return 0;
 }
